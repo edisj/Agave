bf7f22cfa9f919dbc892a690882c5ad14bf0eb24
edisj <ejakupov@asu.edu>
2020-12-16 06:02:17 -0700

    full io

--- benchmarks/2-full_IO/scripts/job_submit.sh
+++ benchmarks/2-full_IO/scripts/job_submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-mkdir -p /scratch/ejakupov/Agave/benchmarks/2-full_node/results/$1/slurm_output
-cd /scratch/ejakupov/Agave/benchmarks/2-full_node/results/$1/slurm_output
+mkdir -p /scratch/ejakupov/Agave/benchmarks/2-full_IO/results/$1/slurm_output
+cd /scratch/ejakupov/Agave/benchmarks/2-full_IO/results/$1/slurm_output
 
 for repeat in 1 2 3
 do
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/1node1.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/1node28.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/3-full_node/scripts/2node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/4node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/6node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/8node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/12node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_node/scripts/16node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/1node1.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/1node28.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/2node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/4node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/6node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/8node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/12node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/16node.sh $1 $repeat
 done
