79ccf5ddc018dda49e235380cfd947126a67fb17
edisj <>
2020-12-22 06:35:56 -0700

    fixed filename

--- benchmarks/in_memory_1/scripts/job_submit.sh
+++ benchmarks/in_memory_1/scripts/job_submit.sh
@@ -7,10 +7,10 @@ for repeat in 1 2 3
 do
     sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/1node1.sh $1 $repeat
     sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/1node28.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/2node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/4node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/6node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/8node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/12node.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/16node.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/2node56.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/4node84.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/6node168.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/8node224.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/12node336.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/in_memory_1/scripts/16node448.sh $1 $repeat
 done
