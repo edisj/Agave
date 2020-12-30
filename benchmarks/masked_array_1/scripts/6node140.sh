8e1ee61a144af9fdc6b44c0d163466dcb0e23b9f
edisj <>
2020-12-30 06:00:55 -0700

    masked array scripts

---
+++ benchmarks/masked_array_1/scripts/6node140.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -N 6  # number of nodes
+#SBATCH -n 168  # number of cores
+#SBATCH -t 0-01:30:00   # time in d-hh:mm:ss
+#SBATCH -p parallel       # partition
+#SBATCH -C Broadwell
+#SBATCH -q normal       # QOS
+#SBATCH -o 6node140.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e 6node140.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=ejakupov@asu.edu # Mail-to address
+
+#echo commands to stdout
+set -x
+
+module purge
+module load anaconda/py3
+module load hdf5/1.10.1-gnu-stock-openmpi-3.0.0
+source activate mda2
+
+testdir=/scratch/ejakupov/Agave/temp/$SLURM_JOB_DEPENDENCY
+
+export OMP_NUM_THREADS=1
+
+mpiexec -n 140 python -W ignore /scratch/ejakupov/Agave/benchmarks/masked_array_1/scripts/benchmark.py $testdir/YiiP_system.pdb $testdir/YiiP_system_9ns_center100x.h5md $1/6node_$2
+
+rm -r $testdir
