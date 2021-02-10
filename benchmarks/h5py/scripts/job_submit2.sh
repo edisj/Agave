511e15ca510604feee77a3a8c4c8d313dbb05293
edisj <>
2021-02-09 22:19:18 -0700

    additional scripts

--- benchmarks/h5py/scripts/job_submit2.sh
+++ benchmarks/h5py/scripts/job_submit2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-mkdir -p /scratch/ejakupov/Agave/benchmarks/2-full_IO/results/$1/slurm_output
-cd /scratch/ejakupov/Agave/benchmarks/2-full_IO/results/$1/slurm_output
+mkdir -p /scratch/ejakupov/Agave/benchmarks/h5py/results/$1/slurm_output
+cd /scratch/ejakupov/Agave/benchmarks/h5py/results/$1/slurm_output
 
 for repeat in 1 2 3
 do
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/2node28.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/4node84.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/6node140.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/8node196.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/12node308.sh $1 $repeat
-    sbatch /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/2-full_IO/scripts/16node420.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/h5py/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/h5py/scripts/1node14.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/h5py/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/h5py/scripts/2node42.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/h5py/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/h5py/scripts/4node98.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/h5py/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/h5py/scripts/6node154.sh $1 $repeat
 done
