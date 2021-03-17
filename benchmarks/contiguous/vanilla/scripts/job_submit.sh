a6454aa810759846ddcaebdb3fb69229e6f7fc82
edisj <>
2021-03-17 02:49:59 -0700

    new file benchmarks

---
+++ benchmarks/contiguous/vanilla/scripts/job_submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/results/$1/slurm_output
+cd /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/results/$1/slurm_output
+
+for repeat in 1 2 3
+do
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/1node1.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/1node4.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/1node8.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/1node14.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/1node28.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/2node56.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/4node112.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/6node168.sh $1 $repeat
+    sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/8node224.sh $1 $repeat
+    #sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/12node336.sh $1 $repeat
+    #sbatch /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/copy_file.sh /scratch/ejakupov/Agave/benchmarks/contiguous/vanilla/scripts/16node448.sh $1 $repeat
+done
