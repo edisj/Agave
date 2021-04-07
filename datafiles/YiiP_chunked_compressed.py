e38c3045937d0947fa5b23e6eb3cdae6d8729890
edisj <>
2021-04-07 12:33:43 -0700

    gzip

--- datafiles/YiiP_chunked_compressed.py
+++ datafiles/YiiP_chunked_compressed.py
@@ -5,7 +5,7 @@ traj = '/scratch/ejakupov/Agave/datafiles/YiiP_system_9ns_center100x_chunked.h5m
 
 u = mda.Universe(top, traj)
 
-with mda.Writer("YiiP_system_9ns_center100x_chunked.h5md",
+with mda.Writer("YiiP_system_9ns_center100x_chunked_gzip.h5md",
                 n_atoms=u.trajectory.n_atoms,
                 n_frames=u.trajectory.n_frames,
                 positions=True,
