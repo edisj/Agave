6e7ab2f057903ecc2539439a85a05946ba972f3c
edisj <>
2021-04-22 10:17:19 -0700

    more compressed

---
+++ datafiles/YiiP_chunked_compressed9.py
@@ -0,0 +1,16 @@
+import MDAnalysis as mda
+
+top = '/scratch/ejakupov/Agave/datafiles/YiiP_system.pdb'
+traj = '/scratch/ejakupov/Agave/datafiles/YiiP_system_9ns_center100x_chunked.h5md'
+
+u = mda.Universe(top, traj)
+
+with mda.Writer("YiiP_system_9ns_center100x_chunked_gzip.h5md",
+                n_atoms=u.trajectory.n_atoms,
+                n_frames=u.trajectory.n_frames,
+                positions=True,
+                chunks=(1,u.trajectory.n_atoms,3),
+                compression="gzip",
+                compression_opts=9) as W:
+    for ts in u.trajectory:
+        W.write(u)
