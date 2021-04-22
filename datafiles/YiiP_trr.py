0d1d26063e0ca861b2404a5b76b66510db0e183e
edisj <>
2021-04-22 13:48:51 -0700

    other formats

---
+++ datafiles/YiiP_trr.py
@@ -0,0 +1,12 @@
+import MDAnalysis as mda
+
+top = '/scratch/ejakupov/Agave/datafiles/YiiP_system.pdb'
+traj = '/scratch/ejakupov/Agave/datafiles/YiiP_system_9ns_center100x_contiguous.h5md'
+
+u = mda.Universe(top, traj)
+
+with mda.Writer("YiiP_system_9ns_center100x_contiguous.trr",
+                n_atoms=u.trajectory.n_atoms,
+                positions=True) as W:
+    for ts in u.trajectory:
+        W.write(u)
---
+++ datafiles/YiiP_xtc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -N 1  # number of nodes
+#SBATCH -n 1  # number of cores
+#SBATCH -t 0-3:00:00   # time in d-hh:mm:ss
+#SBATCH -p serial       # partition
+#SBATCH -q normal       # QOS
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=ejakupov@asu.edu # Mail-to address
+
+module purge
+module load anaconda/py3
+module load hdf5/1.10.1-gnu-stock-openmpi-3.0.0
+source activate mda2
+
+python YiiP_xtc.py
